3991bcb847ea3c7c89a1acf1e8df26aaaefe2e8d
spyle23 <sampingyen.lee@gmail.com>
2022-04-11 18:39:29 +0300

    routes children

--- src/app/pages/page-layout/page-layout-routing.module.ts
+++ src/app/pages/page-layout/page-layout-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageLayoutComponent } from './page-layout.component';
 
 const routes: Routes = [
-  {path:'loan', loadChildren: () => import('../page-loan/page-loan.module').then(m => m.PageLoanModule)}
+  {path:'', component:PageLayoutComponent, children:[
+    {path:'loan', loadChildren: () => import('../page-loan/page-loan.module').then(m => m.PageLoanModule)}
+  ]}
 ];
 
 @NgModule({
