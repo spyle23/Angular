03b9ab6c57ecd37083c8773863624652471ab482
spyle23 <sampingyen.lee@gmail.com>
2022-05-01 12:52:38 +0300

    just configure the form

--- src/app/features/loan/loan.module.ts
+++ src/app/features/loan/loan.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoanCreateComponent } from './loan-create/loan-create.component';
 import { LoanListComponent } from './loan-list/loan-list.component';
 import { LoanCalendarComponent } from './loan-calendar/loan-calendar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +14,8 @@ import { LoanCalendarComponent } from './loan-calendar/loan-calendar.component';
    LoanCalendarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     LoanCreateComponent,
